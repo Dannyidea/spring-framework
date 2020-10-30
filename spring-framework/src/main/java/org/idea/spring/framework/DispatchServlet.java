c17a2c074cb53d7da361159eed7ecb5013711024
DannyIdea <linhao997@qq.com>
2020-10-30 08:48:27 +0800

    version-1 dispatchservlet

--- spring-framework/src/main/java/org/idea/spring/framework/DispatchServlet.java
+++ spring-framework/src/main/java/org/idea/spring/framework/DispatchServlet.java
@@ -1,12 +1,20 @@
 package org.idea.spring.framework;
 
+import org.idea.spring.framework.action.TestController;
+import org.idea.spring.framework.common.RequestMapping;
+import org.idea.spring.framework.common.RestController;
+import sun.misc.Request;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.midi.Soundbank;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @author linhao
@@ -17,22 +25,27 @@ public class DispatchServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doPost(req,resp);
+        this.doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("do post");
-        String url = req.getRequestURI();
-        System.out.println(url);
-        //localhost:8080/mvc/test?name=idea&&id=1
-        String path = req.getContextPath();
-        url.replaceAll(path,"").replaceAll("//?+","");
-        System.out.println(path);
-        Map<String,String[]> paramMap = req.getParameterMap();
-        for (String paramItem : paramMap.keySet()) {
-            System.out.println(paramItem);
-            System.out.println(paramMap.get(paramItem));
+        String url = req.getRequestURI().replaceAll(req.getContextPath(), "");
+        RequestMapping requestMapping = TestController.class.getAnnotation(RequestMapping.class);
+        Pattern pattern = Pattern.compile("/tests*");
+        if (pattern.matcher(requestMapping.url()).matches()) {
+            Method[] methods = TestController.class.getDeclaredMethods();
+            for (Method method : methods) {
+                String reqMethodUrl = url.replaceAll(requestMapping.url(),"");
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    String methodUrl = method.getAnnotation(RequestMapping.class).url();
+                    Pattern methodPattern = Pattern.compile(methodUrl);
+                    if(methodPattern.matcher(reqMethodUrl).matches()){
+                        System.out.println("method name is "+method.getName());
+                        break;
+                    }
+                }
+            }
         }
     }
 
